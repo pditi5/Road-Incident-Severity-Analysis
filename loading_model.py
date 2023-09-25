35c160210d20dfa3d1724404584e74a12a98e010
diti <pditic05@gmail.com>
2023-09-25 12:28:33 +0530

    loading_model.py

--- loading_model.py
+++ loading_model.py
@@ -10,7 +10,7 @@ def get_model(model_path):
     except:
         print("Cannot fetch model from local downloading from drive")
         if not 'RTA_severity model.pkl' in os.listdir('.'):
-            # example url: "https://drive.google.com/u/1/uc?id=18IxYOI-whucBTZmt5qTvvYgjlxleaSqO&export=download&confirm=t"
+            
             url = "https://drive.google.com/u/0/uc?id=1eZQFdqwAMHLsjwD2C-PyvcZlVAwmAudD&export=download&confirm=t"
             r = requests.get(url, allow_redirects=True)
             open(r"RTA_severity model.pkl", 'wb').write(r.content)
