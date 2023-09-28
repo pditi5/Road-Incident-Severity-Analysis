006b6a5a16f6a7611eada1c094b1adfaece6117e
diti <pditic05@gmail.com>
2023-09-28 14:36:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from Prediction import get_prediction, LabelEncoder
 from load_model import get_model
 
-rf_model=get_model(model_path = 'RTA_severity model.pkl')
+rf_model=get_model(model_path = 'random_forest_final.joblib')
 
 st.set_page_config(page_title="Accident Severity Prediction App",
                    page_icon=" 🚧 ", layout="wide")
